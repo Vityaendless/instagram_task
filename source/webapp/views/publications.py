2f77bf4475ce82ef2bdbae068324bf466ee2e578
Vitya Endless <dvitya031190@gmail.com>
2024-06-15 19:18:40 +0600

    some fixes

--- source/webapp/views/publications.py
+++ source/webapp/views/publications.py
@@ -19,8 +19,6 @@ class PublicationCreateView(UserPassesTestMixin, CreateView):
         # return redirect('webapp:publication', pk=publication.pk)
         return redirect('accounts:login')
 
-    #правильная переадресация
-
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
         context['search_form'] = SearchForm(self.request.GET)
